1ecf289174f5d1eb62c9acede7891d251e176ad2
jagruti803 <jagrutiasawale@gmail.com>
2023-04-07 18:29:56 +0530

    new changes to comit

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-cfrom flask import Flask, render_template, request,jsonify
+import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
